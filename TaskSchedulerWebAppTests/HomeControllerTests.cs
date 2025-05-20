ed279ffa20c4cc111a278fe9ce55144eb974aa10
Issac Paulose <71964564+issac07@users.noreply.github.com>
2025-05-19 23:23:49 -0400

    Test solutions added

---
+++ TaskSchedulerWebAppTests/HomeControllerTests.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskSchedulerWebAppTests;
+
+public class HomeControllerTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Fail();
+    }
+}
